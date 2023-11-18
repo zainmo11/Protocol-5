ad61939770a59c9cfbe04ee69cec433f1681f6b3
Abdelrhman <zyn6681@gmail.com>
2023-11-19 00:27:09 +0200

    Base protocol for assignment

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/zyn66/CLionProjects/untitled29
+# Install script for directory: C:/Users/zyn66/CLionProjects/Protocol 5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/zyn66/CLionProjects/untitled29/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/zyn66/CLionProjects/Protocol 5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
