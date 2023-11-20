affa95d2c49cd59f7dd1f10bbd6c30294b36d163
Abdallahyx <99212200+Abdallahyx@users.noreply.github.com>
2023-11-21 01:46:42 +0200

    Add files via upload

--- protocol.c
+++ protocol.c
@@ -1,18 +1,17 @@
 #include "protocol.h"
 #include <stdio.h>
 #include <unistd.h>
-#include <stdbool.h>
-
+#define sleep_time 1000000
 // Assuming timer is a global variable
 boolean timer = false;
 
 void wait_for_event(event_type *event) {
-    printf("Waiting for event\n");
+    printf("Waiting for event");
 
     // Loop to print dots every second for a total of 3 seconds
     for (int i = 0; i < 3; ++i) {
         fflush(stdout);  // Flush the output buffer to ensure immediate printing
-        sleep(1);        // Sleep for 1 second
+        usleep(sleep_time);        // Sleep for 1 second
         printf(".");
     }
     printf("\n");
@@ -20,27 +19,27 @@ void wait_for_event(event_type *event) {
 
 void is_received(event_type e, seq_nr frame_expected) {
     e == frame_arrival ? printf("Data %i Received\n", frame_expected) : printf("Data %i not received\n", frame_expected);
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void from_network_layer(packet p) {
     printf("Take Packet %s from network layer to physical layer\n", p.data);
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void to_network_layer(packet p) {
     printf("Take Packet %s from physical layer to network layer\n", p.data);
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void from_physical_layer(frame r) {
     printf("Take frame %s to network layer\n", r.info.data);
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void to_physical_layer(frame s) {
     printf("Take frame %s to network layer\n", s.info.data);
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void start_timer(seq_nr k) {
@@ -51,29 +50,29 @@ void start_timer(seq_nr k) {
 void stop_timer(seq_nr k) {
     printf("End Timer for sequence number %i\n", k);
     timer = false;
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void start_ack_timer(void) {
     printf("Start Acknowledge Time for sequence number\n");
     timer = true;
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void stop_ack_timer(void) {
     printf("End Acknowledge Time for sequence number\n");
     timer = false;
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void enable_network_layer(void) {
     printf("Enable Network layer\n");
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void disable_network_layer(void) {
     printf("Disable Network layer\n");
-    sleep(1);
+    usleep(sleep_time);
 }
 
 void displayEvent(event_type event) {
